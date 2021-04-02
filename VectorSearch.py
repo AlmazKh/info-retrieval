97338b522516c21d1b54bf27ab2fd1cc82267411
GuzelMusina <guzmus@rambler.ru>
2021-04-02 13:35:37 +0300

    modify

--- VectorSearch.py
+++ VectorSearch.py
@@ -16,7 +16,7 @@ with open("index.txt", "r") as index:
     lines = index.readlines()
     numb = [line[: line.find(" ")] for line in lines]
     for i in range(1, 101):
-        docs_numb[numb[i-1]] = i
+        docs_numb[numb[i - 1]] = i
 
 
 def create_matrix_and_query(req):
@@ -52,42 +52,36 @@ def refactor_matrix_with_k(U, S, V):
     return Uk_, Sk_1_, Vk_T_
 
 
-def create_resp_with_sim(requests):
-    top = Element('responses')
+def create_resp_with_sim(req):
     table_with_sim = dict()
-    for req in requests:
-        clean_req = remove_stopwords(get_normal_form(sanitize(req)))
-        matrix, q = create_matrix_and_query(clean_req)
-        child = SubElement(top, "request", dict({'value': req}))
-        U, S, V = svd_matrix(matrix)
-        Uk, Sk_1, Vk_T = refactor_matrix_with_k(U, S, V)
-        new_q = np.dot(q, Uk).dot(Sk_1)
-        for i in range(100):
-            score_first = 0
-            multipl_q = 0
-            multipl_d = 0
-            sim = 0
-            for j in range(len(new_q)):
-                score_first += new_q[j] * Vk_T[j][i]
-                multipl_q += pow(new_q[j], 2)
-                multipl_d += pow(Vk_T[j][i], 2)
-                sim = score_first / math.sqrt(multipl_q) / math.sqrt(multipl_d)
-            table_with_sim.update({i + 1: sim})
-        sorted_dict = {k: v for k, v in sorted(table_with_sim.items(), key=lambda it_: it_[1], reverse=True)}
-        number = 1
-        sorted_dict_iter = iter(sorted_dict)
-        for el in sorted_dict_iter:
-            if number > 10:
-                break
-            SubElement(child, "response",
-                       dict({'number': str(number), 'sim': str(table_with_sim.get(el)),
-                             'url': str(get_doc_url()[numb[el + 1]])}))
-            number += 1
+    clean_req = remove_stopwords(get_normal_form(sanitize(req)))
+    matrix, q = create_matrix_and_query(clean_req)
+    U, S, V = svd_matrix(matrix)
+    Uk, Sk_1, Vk_T = refactor_matrix_with_k(U, S, V)
+    new_q = np.dot(q, Uk).dot(Sk_1)
+    for i in range(100):
+        score_first = 0
+        multipl_q = 0
+        multipl_d = 0
+        sim = 0
+        for j in range(len(new_q)):
+            score_first += new_q[j] * Vk_T[j][i]
+            multipl_q += pow(new_q[j], 2)
+            multipl_d += pow(Vk_T[j][i], 2)
+            sim = score_first / math.sqrt(multipl_q) / math.sqrt(multipl_d)
+        table_with_sim.update({i + 1: sim})
+    sorted_dict = {k: v for k, v in sorted(table_with_sim.items(), key=lambda it_: it_[1], reverse=True)}
+    sorted_dict_iter = iter(sorted_dict)
+    result_url_list = []
+    for el in sorted_dict.values():
+        result_url_list.append(get_doc_url()[numb[int(el + 1)]])
 
-    w = open("svd_lsi.xml", "w", encoding="utf-8")
-    print(prettify(top), file=w)
+    df = pd.DataFrame(result_url_list)
+    st.table(df)
 
 
-requests = web_search()
-
-create_resp_with_sim(requests)
+st.title('Поисквая система')
+title = st.text_input('Что найти?', 'Слово')
+if st.button('Найти'):
+    st.write('Идет поиск.... по ...', title)
+    create_resp_with_sim(title)
